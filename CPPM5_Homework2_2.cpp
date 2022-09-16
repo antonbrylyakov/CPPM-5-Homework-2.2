518af47443fe3a02ff2a9833397ba7e70bc7e586
Anton Brylyakov <yoll@rambler.ru>
2022-09-15 19:21:01 -0700

    Initial commit

---
+++ CPPM5_Homework2_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+
+struct Account
+{
+    unsigned long long number;
+    std::string clientName;
+    double balance;
+};
+
+void printAccountSummary(Account account)
+{
+    std::cout << "Ваш счёт: " << account.clientName << ", " << account.number << ", " << account.balance;
+}
+
+void setBalance(Account& account, double newBalance)
+{
+    account.balance = newBalance;
+}
+
+int main()
+{
+    Account account;
+    setlocale(LC_ALL, "Russian");
+    // Задаем кодировку для ввода текста с консоли только для Windows!
+    SetConsoleCP(1251);
+    std::cout << "Введите номер счёта: ";
+    std::cin >> account.number;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> account.clientName;
+    std::cout << "Введите баланс: ";
+    std::cin >> account.balance;
+
+    double newBalance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newBalance;
+    setBalance(account, newBalance);
+    printAccountSummary(account);
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Задача 2. Счета
+[Условие задачи](https://github.com/netology-code/cppm-homeworks/tree/main/02/02)
